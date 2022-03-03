1258fe56ee005b5e32f6b425928fbf3977e23ef7
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-03 16:30:12 +0000

    adds exhaustion question data

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const Exhaustion = [
+  
+  {
+    title: "Question 1",
+    question: "At work, I feel mentally exhausted"
+  },
+  {
+    title: "Question 2",
+    question: "Everything I do at work requires a great deal of effort"
+  },
+  {
+    title: "Question 3",
+    question: "After a day at work, I find it hard to recover my energy"
+  },
+  {
+    title: "Question 4",
+    question: "At work, I feel physically exhausted"
+  },
+  {
+    title: "Question 5",
+    question: "When I get up in the morning, I lack the energy to start a new day at work"
+  },
+  {
+    title: "Question 6",
+    question: "I want to be active at work, but somehow, I am unable to manage"
+  },
+  {
+    title: "Question 7",
+    question: "When I exert myself at work, I quickly get tired"
+  },
+  {
+    title: "Question 8",
+    question: "At the end of my working day, I feel mentally exhausted and drained"
+  }
+]
