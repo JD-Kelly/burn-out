6e1368f637e9840ddb7cf51c7ee538285c72ac1f
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-03 17:54:20 +0000

    adds button to start questionaire

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import './App.css';
 import Header from "./Components/Header"
 
 function App() {
+  const [questionnaireStarted, setQuestionnaireStarted]=useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQuestionnaireStarted(true)
+  }
+
   return (
     <div className="App">
-      <Header />
+      <Header handleSubmit={handleSubmit} questionnaireStarted={questionnaireStarted}/>
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Header = () => {
-  return (
-    <div>
-      <h1>Welcome to Burnout</h1>
-    </div>
-  )
+
+const Header = ({ handleSubmit, questionnaireStarted}) => {
+  if(questionnaireStarted === true) {
+   return (
+      <h1>Here is question 1</h1>
+   )
+  } else {
+    return (
+      <div>
+        <h1>Welcome to Burnout</h1>
+        <button onClick={handleSubmit}>Start Questionnaire</button>
+      </div>
+    )
+  }
 }
+  
 
 export default Header;
