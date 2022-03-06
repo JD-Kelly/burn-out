1827157cc73f90f3f4513ec5d1daae4d85bf2bab
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-06 13:26:03 +0000

    adds basic scoring feedback

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -12,8 +12,17 @@ const Result = ({ score, setScore, setQuestionnaireStarted, setQuestionnaireFini
 
   return (
     <div>Your Results
-    <h3>{sum}</h3>
-    <button onClick={handleOnClick}>Restart Questionnaire</button>
+      <h3>{sum}</h3>
+      <button onClick={handleOnClick}>Restart Questionnaire</button>
+      <h3>
+        Scored 8-13 : Low to no risk of burnout
+      </h3>
+      <h3>
+        Scored 14-26 : Moderate risk of burnout 
+      </h3>
+      <h3>
+        Scored 27-40 : Moderate to high risk of burnout
+      </h3>
     </div>
   )
 }
