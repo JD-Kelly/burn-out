ab7ff13623f838b649a9d19286fb53f0dee5cd4f
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-06 13:16:03 +0000

    clears user score when questionnaire reset

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Result = ({ score, setQuestionnaireStarted, setQuestionnaireFinished }) => {
+const Result = ({ score, setScore, setQuestionnaireStarted, setQuestionnaireFinished }) => {
 
   const sum = score.reduce((a, b) => a + b)
 
   const handleOnClick = () => {
+    setScore([])
     setQuestionnaireStarted(false)
     setQuestionnaireFinished(false)
   }
