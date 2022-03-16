4d6c735e3080ae321be4bf1b26d5b0356bd5b1c4
Jonny Kelly <jonny.kelly192@gmail.com>
2022-03-16 11:53:03 +0000

    adds rest of data for questionnaire

--- src/data.js
+++ src/data.js
@@ -31,5 +31,65 @@ export const Questions = [
   {
     title: "Question 8",
     question: "At the end of my working day, I feel mentally exhausted and drained"
-  }
+  },
+  {
+    title: "Question 9",
+    question: "I struggle to find any enthusiasm for my work"
+  },
+  {
+    title: "Question 10",
+    question: "At work, I do not think much about what I am doing and I function on autopilot"
+  },
+  {
+    title: "Question 11",
+    question: "I feel a strong aversion towards my job"
+  },
+  {
+    title: "Question 12",
+    question: "I feel indifferent about my job"
+  },
+  {
+    title: "Question 13",
+    question: "I’m cynical about what my work means to others"
+  },
+  {
+    title: "Question 14",
+    question: "At work, I have trouble staying focused"
+  },
+  {
+    title: "Question 15",
+    question: "At work I struggle to think clearly"
+  },
+  {
+    title: "Question 16",
+    question: "I’m forgetful and distracted at work"
+  },
+  {
+    title: "Question 17",
+    question: "When I’m working, I have trouble concentrating"
+  },
+  {
+    title: "Question 18",
+    question: "I make mistakes in my work because I have my mind on other things"
+  },
+  {
+    title: "Question 19",
+    question: "At work, I feel unable to control my emotions"
+  },
+  {
+    title: "Question 20",
+    question: "I do not recognize myself in the way I react emotionally at work"
+  },
+  {
+    title: "Question 21",
+    question: "During my work I become irritable when things don’t go my way"
+  },
+  {
+    title: "Question 22",
+    question: "I get upset or sad at work without knowing why"
+  },
+  {
+    title: "Question 23",
+    question: "At work I may overreact unintentionally"
+  },
 ]
